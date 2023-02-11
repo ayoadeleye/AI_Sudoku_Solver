5c7d505da5400bf16c42ee5195f652cd95d9b8e6
Ayo <ayooluwaadeleye17@gmail.com>
2023-02-11 17:37:37 -0600

    POST and save image to flask server

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+from flask import Flask, render_template, request, flash, redirect
+import os
+from werkzeug.utils import secure_filename
+import cv2
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'C:/Users/ayool/Documents/GitHub/AI_Sudoku_Solver/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg', 'gif']
+
+app.config['SECRET_KEY'] = '9a716eae4129c7b4a79e69ae1bb5d7dc'
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-# Press the green button in the gutter to run the script.
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/", methods=['POST'])
+def upload_image():
+    image = request.files["image"]
+    if image.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if image and allowed_file(image.filename):
+        filename = secure_filename(image.filename)
+        full_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        print('save')
+        image.save(full_path)
+
+        flash('Image successfully uploaded and displayed')
+
+    return render_template("index.html")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(debug=True)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
